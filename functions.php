56badf62c329668ea703392856fbddec1a38fb1e
shaunBoahCodes <131640457+shaunBoahCodes@users.noreply.github.com>
2023-08-20 21:15:09 +0800

    Added tailwind

--- functions.php
+++ functions.php
@@ -1,12 +1,14 @@
 <?php 
 
-function theme_styles() {
-    wp_enqueue_style( 'main_css', get_template_directory_uri() . '/src/assets/main.css' );
-}
-add_action( 'wp_enqueue_scripts', 'theme_styles' );
-
 function theme_js() {
     //To avoid caching I use time, remove it in production.    
     wp_enqueue_script('vue', get_template_directory_uri() . '/dist/bundle.js', '', '1.0.0', true);
 }
 add_action( 'wp_enqueue_scripts', 'theme_js' );
+
+function theme_styles() {
+    wp_enqueue_style( 'main_css', get_template_directory_uri() . '/src/assets/main.css' );
+    
+    wp_enqueue_style( 'tailwind_css', get_template_directory_uri() . '/src/assets/tailwind.css' );
+}
+add_action( 'wp_enqueue_scripts', 'theme_styles' );
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,37 +8,12 @@ defineProps({
 </script>
 
 <template>
-  <div class="greetings">
+  <div class="bg-blue-600">
     <h1 class="green">{{ msg }}</h1>
-    <h3>
+    <h3 class="bg-black">
       You’ve successfully created a project
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
   </div>
 </template>
-
-<style scoped>
-h1 {
-  font-weight: 100;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
