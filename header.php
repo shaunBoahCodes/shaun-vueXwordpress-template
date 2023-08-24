077784033b9a0ec1b1d2a760eb756d571fbef9c6
shaunBoahCodes <131640457+shaunBoahCodes@users.noreply.github.com>
2023-08-24 20:51:26 +0800

    fixed height and title

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>shaunlaclemence.local</title>
+	<title>Vue-Wordpress-Template</title>
 	<?php wp_head(); ?> 
 </head>
 <body>
